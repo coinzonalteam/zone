80777224f0e5ed9878e4fa38407d8b7417bfac11
Streple Duck <streple@streple>
2025-07-03 05:36:38 -0400

    all

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 dbPassword33 = 'mongodb://localhost:27017/mern2';
-dbPassword = 'mongodb+srv://cryptomartic:TriedPass12@cluster0.sjnko.mongodb.net/CryptoMarticUsers'
+dbPassword = 'mongodb+srv://coinzonalteam:ZoningPassed.22@cluster0.5w38fmt.mongodb.net/CoinzonalUsers'
 module.exports = {
     mongoURI: dbPassword
 };
